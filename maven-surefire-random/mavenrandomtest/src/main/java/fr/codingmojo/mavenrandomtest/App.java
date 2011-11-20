0100b9bf5a998ed9f68f058e3e55eb79544ccaaf
Ludovic Meurillon <ludovic.meurillon@gmail.com>
2011-11-20 14:49:41 +0100

    first coding mojo commit

---
+++ maven-surefire-random/mavenrandomtest/src/main/java/fr/codingmojo/mavenrandomtest/App.java
@@ -0,0 +1,13 @@
+package fr.codingmojo.mavenrandomtest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
